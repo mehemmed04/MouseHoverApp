3e6313052783ef7fe899f194142e0ed71c32b760
Mehemmed <mehemmedbayramov2004@gmail.com>
2022-07-23 23:38:45 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLBL = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // questionLBL
+            // 
+            this.questionLBL.AutoSize = true;
+            this.questionLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLBL.Location = new System.Drawing.Point(294, 62);
+            this.questionLBL.Name = "questionLBL";
+            this.questionLBL.Size = new System.Drawing.Size(233, 31);
+            this.questionLBL.TabIndex = 0;
+            this.questionLBL.Text = "Are you stupid ? ";
+            this.questionLBL.Click += new System.EventHandler(this.questionLBL_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(200, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "YES";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(502, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "NO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.questionLBL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label questionLBL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+        public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I knew!");
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questionLBL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            if(sender is Button bt)
+            {
+                int RandomX = random.Next(this.Width - 135);
+                int RandomY = random.Next(this.Height - 135);
+                bt.Location = new Point(RandomX,RandomY);
+                int randomColor = random.Next(Colors.colors.Length);
+                this.BackColor =Colors.colors[randomColor] ;
+            }
+        }
+    }
+    public class Colors
+    {
+        public static Color[] colors = new Color[] {
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+            Color.White,
+            Color.Aqua,
+            Color.Brown,
+            Color.Yellow,
+            Color.Chocolate
+        };
+        public Color this[int index] => colors[index];
+
+    }
+}
